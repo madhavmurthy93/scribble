3abeccce9a0a819ef7b777b5dde5c8f03e1e3b44
Madhav Murthy <madhavmurthy93@gmail.com>
2024-07-10 17:11:46 +0530

    Implement all get methods and corresponding views

--- routes/api.js
+++ routes/api.js
@@ -23,19 +23,34 @@ db.removeAsync = promisify(db.remove);
 const router = express.Router();
 
 // Get all scribbles irrespective of the user
-router.get("/scribbles", (req, res) => {
-    res.sendStatus(200);
+router.get("/scribbles", async (req, res) => {
+    try {
+        const docs = await db.findAsync({});
+        console.log("Scribbles found:", docs);
+        res.status(200).json({ docs: docs });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: "Failed to find scribble"});
+    }
 });
 
 // Get scribbles by the specified user
-router.get("/:username/scribbles", (req, res) => {
-    res.sendStatus(200);
+router.get("/:username/scribbles", async (req, res) => {
+    try {
+        const docs = await db.findAsync({ username: req.params.username });
+        console.log("Scribbles found:", docs);
+        res.status(200).json({ docs: docs });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: "Failed to find scribble"});
+    }
 });
 
 // Get the specified scribble by user and slug URL
 router.get("/:username/:slug", async (req, res) => {
     try {
         const doc = await db.findOneAsync({ username: req.params.username, slug: req.params.slug });
+        console.log("Scribble found:", doc);
         res.status(200).json(doc);
     } catch (err) {
         console.log(err);
@@ -61,7 +76,7 @@ router.post("/scribble", async (req, res) => {
             updatedAt: at
         };
         const doc = await db.insertAsync(scribble);
-        console.log("Scribble inserted: ", doc);
+        console.log("Scribble inserted:", doc);
 
         // send a created status code, with a redirect Url in the header; browser redirects to the home page
         const scribbleUri = `/${body.username}/${slug}`;
--- routes/views.js
+++ routes/views.js
@@ -5,25 +5,43 @@ const router = express.Router();
 const server = "http://localhost:3000";
 
 // Render home page with all posts
-router.get("/", (req, res) => {
-    res.render("home");
-});
-
-// Render user page with all posts by user
-router.get("/:username", (req, res) => {
-    res.sendStatus(200);
+router.get("/", async (req, res) => {
+    try {
+        const docs = (await axios.get(`${server}/api/scribbles`)).data;
+        res.render("scribbles", docs);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: "Failed to find scribbles"});
+    }
 });
 
 // Render the create new scribble page 
 router.get("/scribble", (req, res) => {
-    res.sendStatus(200);
+    res.render("scribble-create");
+});
+
+// Render user page with all posts by user
+router.get("/:username", async (req, res) => {
+    try {
+        const docs = (await axios.get(`${server}/api/${req.params.username}/scribbles`)).data;
+        res.render("scribbles", docs);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: "Failed to find scribbles"});
+    }
 });
 
 // Render the specified scribble
 router.get("/:username/:slug", async (req, res) => {
     try {
-        const doc = await axios.get(`${server}/api/${req.params.username}/${req.params.slug}`);
-        res.status(200).json(doc.data);
+        const doc = (await axios.get(`${server}/api/${req.params.username}/${req.params.slug}`)).data;
+        res.render("scribble", {
+            title: doc.title,
+            gist: doc.gist,
+            username: doc.username,
+            createdAt: doc.createdAt,
+            scribble: doc.scribble
+        });
     } catch (err) {
         console.log(err);
         res.status(500).json({error: "Failed to find scribble"});
